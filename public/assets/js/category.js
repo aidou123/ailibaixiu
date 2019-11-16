1ca5e67b3652e6a997238456a810a366940525db
jiangxiaojun <2388660779@qq.com>
2019-11-16 15:41:55 +0800

    分类列表修改和删除功能完成

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -20,3 +20,41 @@ $.ajax({
         $('#categoryBox').html(html)
     }
 });
+// 为编辑按钮添加点击事件
+$('#categoryBox').on('click','.edit',function () {
+    let id = $(this).attr('data-id')
+    $.ajax({
+        type: "get",
+        url: `/categories/${id}`,
+        success: function (response) {
+            let html = template('modifyCategoryTpl',response)
+            $('#formBox').html(html)
+        }
+    });
+})
+// 当修改分类数据表单发生提交的时候
+$('#formBox').on('submit','#modifyCategory',function () {
+    let formData = $(this).serialize();
+    let id = $(this).attr('data-id');
+    $.ajax({
+        type: "put",
+        url: `/categories/${id}`,
+        data: formData,
+        success: function () {
+            location.reload()
+        }
+    });
+})
+
+$('#categoryBox').on('click','.delete',function () {
+    if(confirm('你确定要删除当前分类吗？')) {
+        let id = $(this).attr('data-id');
+        $.ajax({
+            type: "delete",
+            url: `/categories/${id}`,
+            success: function () {
+                location.reload()
+            }
+        });
+    }
+})
